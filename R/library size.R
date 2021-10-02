ae892fb16a1bc4ca8ea39cac5d49888a3d4c5df6
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-02 16:50:31 +1000

    Adding R script with function to determine library size

---
+++ R/library size.R
@@ -0,0 +1,12 @@
+# determining library size
+
+library.size <- function(data1,data2,plot_type){
+  sample.info <-  as.data.frame(SummarizedExperiment::colData(data1))
+  library_size <- log2(colSums(data2))
+  if (plot_type == "Boxplot"){
+    boxplot(library_size ~ sample.info$year_mda)
+  } else
+    if (plot_type == "Scatterplot"){
+      plot(library_size, xlab = 'sample', ylab = 'log2 library size', col = factor(sample.info$year_mda))
+    }
+}
