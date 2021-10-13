3ea0faa51f767efa81290b8af6ef3f67dddb9b06
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-13 20:36:42 +1100

    Updating filter_low_genes.R script. filter.low.genes function will output S4 data object instead of dataframe. Makes function versatile.

--- R/filter_low_genes.R
+++ R/filter_low_genes.R
@@ -1,13 +1,14 @@
 # Removing lowly expressed genes
 
 filter.low.genes <- function(data,gene_count,sample_size){
+  raw.count <- as.data.frame(SummarizedExperiment::assay(data, 'HTseq_counts'))
   keep.high <- apply(
-    data,
+    raw.count,
     1,
     function(x) length(x[x>gene_count])>=sample_size
   )
-  data.filtered2 <- data[keep.high , ]
-  return(data.filtered2)
+  data.filtered <- data[keep.high , ]
+  return(data.filtered)
 }
 
 #df2 <- filter.low.genes(data=df1,gene_count = 20,sample_size = 200)
