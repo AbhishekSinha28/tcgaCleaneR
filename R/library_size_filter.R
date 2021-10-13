eed3c202d4f9390f39d386c0573be121514c5198
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-13 21:58:21 +1100

    Updating third filter i.e. library.size.fil function with global data input to reduce dependency.

--- R/library_size_filter.R
+++ R/library_size_filter.R
@@ -1,10 +1,11 @@
 # Removing samples based on library size
 
 library.size.fil <- function(data,ls_cutoff){
-  library_size <- log2(colSums(data))
+  raw.count <- as.data.frame(SummarizedExperiment::assay(data, 'HTseq_counts'))
+  library_size <- log2(colSums(raw.count))
   keep.samples <- library_size > ls_cutoff
-  brca.se.filtered3 <- data[ , keep.samples]
-  return(brca.se.filtered3)
+  brca.se.filtered <- data[ , keep.samples]
+  return(brca.se.filtered)
 }
 
 # df4 <- library.size.fil(data = df2, ls_cutoff = 22)
