5381b7d5d76235890819258b926f5d81197d9fca
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-25 23:50:04 +1100

    Adding filter.genes function documentation.

--- R/filter_genes.R
+++ R/filter_genes.R
@@ -1,5 +1,22 @@
 # Filtering Genes Function
 
+#' @title Gene Filter
+#'
+#' @description This function is a part of the data wrangling functionality of tgcapkg.    It allows user to input the TGCA dataset and the required genes to filter the input data based on genes.
+#'
+#' @usage filter.genes(data=brca.se.data.temp,gene.type=c("protein_coding","lncRNA"))
+#'
+#' @param data Input TGCA Dataset.
+#' @param gene.type A character vector of items.
+#'
+#' @return S4 data object
+#' @export
+#'
+#' @examples
+#' filter.genes(data=brca.se.data.temp,gene.type=c("protein_coding","lncRNA"))
+#' \dontrun{
+#' filter.genes(data=brca.se.data.temp,gene.type=c("protein_coding"))
+#' }
 filter.genes <- function(data,gene.type){
   gene.annot.rm <-  as.data.frame(SummarizedExperiment::rowData(data))
   keep.genes.rm <- gene.annot.rm$gene_biotype_BioMart %in% gene.type
@@ -7,4 +24,3 @@ filter.genes <- function(data,gene.type){
   return(data.filtered)
 }
 
-#df1 <- filter.genes(data=brca.se.data.temp,gene.type=c("protein_coding","lncRNA"))
