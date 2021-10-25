fd8f811834c2d3d9371909ac98d7551b827076f0
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-26 01:57:23 +1100

    Updating library.size and filter_low_genes function documentation.

--- R/filter_low_genes.R
+++ R/filter_low_genes.R
@@ -1,5 +1,18 @@
 # Removing lowly expressed genes
 
+#' @title Low Count Genes Filter
+#'
+#' @description This function is a part of the data wrangling functionality of tgcapkg.    It allows user to input the TGCA dataset and the threshold for the minimum gene count and sample count.
+#'
+#' @param data Input TGCA Dataset.
+#' @param gene_count numeric: gene count threshold
+#' @param sample_size numeric: sample size threshold
+#'
+#' @return S4 data object
+#' @export
+#'
+#' @examples
+#' filter.low.genes(data=brca.se.data.temp,gene_count = 20,sample_size = 200)
 filter.low.genes <- function(data,gene_count,sample_size){
   raw.count <- as.data.frame(SummarizedExperiment::assay(data, 'HTseq_counts'))
   keep.high <- apply(
@@ -10,5 +23,3 @@ filter.low.genes <- function(data,gene_count,sample_size){
   data.filtered <- data[keep.high , ]
   return(data.filtered)
 }
-
-#df2 <- filter.low.genes(data=df1,gene_count = 20,sample_size = 200)
---
+++ R/library.size.R
@@ -0,0 +1,30 @@
+# determining library size
+
+#' @title Library Size
+#'
+#' @description This function is a part of the data analysis functionality of tgcapkg.    It allows user to input the TGCA dataset and the type of plot to analyse library size.
+#'
+#' @param data Input TGCA Dataset.
+#' @param plot_type character: Plot type
+#'
+#' @return Scatter Plot, Box plot
+#' @export
+#'
+#' @examples
+#' library.size(data = brca.se.data.temp, plot_type = "Scatterplot")
+#' \dontrun{
+#' library.size(data = brca.se.data.temp, plot_type = "Boxplot")
+#' }
+library.size <- function(data,plot_type){
+  sample.info <-  as.data.frame(SummarizedExperiment::colData(data))
+  raw.count <- as.data.frame(SummarizedExperiment::assay(data, 'HTseq_counts'))
+  library_size <- log2(colSums(raw.count))
+  if (plot_type == "Boxplot"){
+    boxplot(library_size ~ sample.info$year_mda)
+  } else
+    if (plot_type == "Scatterplot"){
+      plot(library_size, xlab = 'sample', ylab = 'log2 library size', col = factor(sample.info$year_mda))
+    }
+}
+
+# df4 <- library.size(data = df3, plot_type = "Scatterplot")
