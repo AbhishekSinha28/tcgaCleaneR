b02c872aa97a9577b239c158242cd2471bedd23c
AbhishekSinha28 <asin0080@student.monash.edu>
2021-10-27 11:20:45 +1100

    Debugging package size warnings for R CMD check.

--- data_raw/tgca_breast_cancer_data.R
+++ data_raw/tgca_breast_cancer_data.R
@@ -1,10 +1,10 @@
 # Load Data
 # Due to data size issues the actual raw data is not included as part of the package. A condensed
-# version of the original dataset containing 180 observations is used as part of the package functionality.
+# version of the original dataset containing 100 observations is used as part of the package functionality.
 brca.se <- base::readRDS(here::here("data_raw","TCGA_SummarizedExperiment_HTseq_BRCA.rds"))
 
 # Data Subset
-brca.data <- brca.se[1:180, ]
+brca.data <- brca.se[1:100, ]
 
 #
 #library(SummarizedExperiment)
@@ -12,7 +12,7 @@ brca.data <- brca.se[1:180, ]
 gene.annot <-  as.data.frame(SummarizedExperiment::rowData(brca.data))
 # Sample level data
 sample.info <-  as.data.frame(SummarizedExperiment::colData(brca.data))
-# Raw and standardize values
+# Raw and normalized values
 raw.count <- as.data.frame(SummarizedExperiment::assay(brca.data, 'HTseq_counts'))
 fpkm <- as.data.frame(SummarizedExperiment::assay(brca.data, 'HTseq_FPKM'))
 fpkm.uq <- as.data.frame(SummarizedExperiment::assay(brca.data, 'HTseq_FPKM.UQ'))
